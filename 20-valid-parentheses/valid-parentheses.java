885cd77a90a73382a319f5ac13d9a586290507d1
Raunak Kumar Singh <138483101+Raunakkumarsingh2005@users.noreply.github.com>
2024-11-07 15:20:10 +0530

    Time: 2 ms (97.13%) | Memory: 41.9 MB (16.81%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean isValid(String s) {
+        char[] a = s.toCharArray();
+        Stack<Character> stack = new Stack<>();
+        if (a.length == 1 ){//|| a[0] == ')' || a[0] == '}' || a[0] == ']'){
+            return false;
+        }
+        for (char i : a){
+            if (i == ')' && !stack.isEmpty()) {
+                if (stack.peek() =='('){
+                    stack.pop();
+                    continue;
+                }
+                return false;
+            }
+
+            if (i == '}' && !stack.isEmpty()) {
+                if (stack.peek() =='{'){
+                    stack.pop();
+                    continue;
+                }
+                return false;
+            }
+            
+            if (i == ']' && !stack.isEmpty()) {
+                if (stack.peek() =='['){
+                    stack.pop();
+                    continue;
+                }
+                return false;
+            }
+
+            else stack.push(i);
+        }
+
+        return stack.isEmpty();
+    }
+}
