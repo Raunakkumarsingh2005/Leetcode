8553d1f8fe58d6418870f4cc68754f7b5d0f22cf
Raunak Kumar Singh <138483101+Raunakkumarsingh2005@users.noreply.github.com>
2025-01-23 18:42:36 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (15.24%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -14,49 +14,26 @@
  * }
  */
 class Solution {
-    // public boolean isValidBST(TreeNode root) {
-    //     return isValid(root, null, null);
-    // }
-
-    // public Boolean isValid(TreeNode node, Integer low, Integer high) {
-    //     if (node == null) {
-    //         return true;
-    //     }
-
-    //     if (low != null && low <= node.val) {
-    //         return false;
-    //     }
-
-    //     if (high != null && high >= node.val) {
-    //         return false;
-    //     }
-
-    //     Boolean left = isValid(node.left, low, node.val);
-    //     Boolean right = isValid(node.right, node.val, high);
-
-    //     return left && right;
-    // }
-
     public boolean isValidBST(TreeNode root) {
-        return helper(root, null, null);
-  }
-  public boolean helper(TreeNode node, Integer low, Integer high) {
-    if (node == null) {
-      return true;
+        return isValid(root, null, null);
     }
 
-    if (low != null && node.val <= low) {
-      return false;
+    public Boolean isValid(TreeNode node, Integer low, Integer high) {
+        if (node == null) {
+            return true;
+        }
+
+        if (low != null && node.val <= low) {
+            return false;
+        }
+
+        if (high != null && node.val >= high) {
+            return false;
+        }
+
+        Boolean left = isValid(node.left, low, node.val);
+        Boolean right = isValid(node.right, node.val, high);
+
+        return left && right;
     }
-
-    if(high != null && node.val >= high) {
-      return false;
-    }
-
-    boolean leftTree = helper(node.left, low, node.val);
-    boolean rightTree = helper(node.right, node.val, high);
-
-    return leftTree && rightTree;
-    
-  }
 }
