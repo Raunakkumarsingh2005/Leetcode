8696ba294e6cd4a6370a9bae98e5fb55f6af941f
Raunak Kumar Singh <138483101+Raunakkumarsingh2005@users.noreply.github.com>
2025-06-24 08:53:41 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (97.70%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
@@ -10,17 +10,28 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        ListNode temp = head;
-        int value;
-        while (temp != null){
-            if (temp.next != null && temp.val == temp.next.val){
-                temp.next = temp.next.next;
+        if (head == null) {
+            return head;
+        }
+
+    
+        ListNode curr, prev;
+        curr = head.next;
+        prev = head;
+        int value = head.val;   
+
+        while (curr != null) {
+            if (curr.val == value) {
+                prev.next = curr.next;
+                curr = curr.next;
+                continue;
             }
-            else 
-            temp = temp.next;
+
+            value = curr.val;
+            prev = prev.next;
+            curr = curr.next;
         }
 
         return head;
-        
     }
 }
