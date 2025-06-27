9b727ae066799439becc4de1a560156aa90d1e4b
Raunak Kumar Singh <138483101+Raunakkumarsingh2005@users.noreply.github.com>
2025-06-27 11:02:58 +0530

    Time: 101 ms (23.17%) | Memory: 52.8 MB (14.65%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,36 +1,42 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-    List<List<Integer>> result = new ArrayList<>();
-    Arrays.sort(nums);  // Sort the array
+        HashSet<List<Integer>> set = new HashSet<>();
+        Arrays.sort(nums);
 
-    for (int i = 0; i < nums.length - 2; i++) {
-        // Skip duplicate elements
-        if (i > 0 && nums[i] == nums[i - 1]) continue;
+        for (int i = 0; i < nums.length-1; i++) {
+            int target = -nums[i];
+            int left = 0;
+            int right = nums.length-1;
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
 
-        int left = i + 1;
-        int right = nums.length - 1;
+            while (left < right) {
+                if (left == i) {
+                    left++;
+                } else if (right == i) {
+                    right--;
+                }
 
-        while (left < right) {
-            int sum = nums[i] + nums[left] + nums[right];
+                else if (left < right) {
+                    int sum = nums[left] + nums[right];
+                    if (sum > target) {
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        List<Integer> arr = new ArrayList<>(Arrays.asList(nums[i], nums[left], nums[right]));
+                        Collections.sort(arr);
+                        set.add(arr);
+                        left++;
+                        right--;
+                    }
+                }
 
-            if (sum == 0) {
-                result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-
-                // Skip duplicates
-                while (left < right && nums[left] == nums[left + 1]) left++;
-                while (left < right && nums[right] == nums[right - 1]) right--;
-
-                left++;
-                right--;
-            } else if (sum < 0) {
-                left++;
-            } else {
-                right--;
             }
         }
+
+        List<List<Integer>> list = new ArrayList(set);
+        return list;
     }
-
-    return result;
-}
-
 }
