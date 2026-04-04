4a51f4ae954085da610f69ae28ae2945e74b781a
Raunak Kumar Singh <kumarsinghraunak27@gmail.com>
2026-04-04 11:10:56 +0530

    Time: 11 ms (5.85%) | Memory: 66.5 MB (96.91%) - LeetSync

---
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxAbsoluteSum(int[] nums) {
+        int bestEnding = nums[0];
+        int result = nums[0];
+        int bestending2 = nums[0];
+
+        if (nums.length <= 1) {
+            result = Math.max(result, Math.max(Math.abs(bestEnding), Math.abs(bestending2)));
+        }
+
+        for (int i = 1 ; i < nums.length; i++) {
+            int v1 = bestEnding + nums[i];
+            int v2 = nums[i];
+            int v3 = bestending2 + nums[i];
+
+            bestEnding = Math.max(v1, Math.max(v2, v3));
+            bestending2 = Math.min(v1, Math.min(v2, v3));
+
+            result = Math.max(result, Math.max(Math.abs(bestEnding), Math.abs(bestending2)));
+        }
+
+        return result;
+    }
+}
