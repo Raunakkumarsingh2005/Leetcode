a7416f592735f52ac3c423da057b00a756ba6d85
Raunak Kumar Singh <kumarsinghraunak27@gmail.com>
2026-04-04 11:28:21 +0530

    Time: 1 ms (22.74%) | Memory: 48.2 MB (65.70%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -1,13 +1,14 @@
 class Solution {
     public void reverseString(char[] s) {
-        for (int i = 0 ; i < s.length/2 ; i++) {
-            swap(s, i, s.length-1-i);
+        
+      int i=0,j=s.length-1;
+        while(i<j){
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
         }
-    }
 
-    public void swap(char[] s, int i, int j) {
-        char temp = s[i];
-        s[i] = s[j];
-        s[j] = temp;
     }
 }
