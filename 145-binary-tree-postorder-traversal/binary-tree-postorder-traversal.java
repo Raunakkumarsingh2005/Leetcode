54eb9e48cfc161caaa91a623f90eb07b0655edb6
Raunak Kumar Singh <kumarsinghraunak27@gmail.com>
2026-05-04 00:16:54 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (31.59%) - LeetSync

---
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        dfs(root, res);
+        return res;
+    }
+
+    private void dfs(TreeNode root, List<Integer> res) {
+        if (root == null) return;
+        dfs(root.left, res);
+        dfs(root.right, res);
+        res.add(root.val);
+    }
+}
