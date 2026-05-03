de3b40a7f7ea1d3b26d32f3f57c38adc5f887039
Raunak Kumar Singh <kumarsinghraunak27@gmail.com>
2026-05-03 21:55:43 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (66.09%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -1,30 +1,19 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-       return binarysearch(nums , target) ;
-    //    if (nums[a] == target){
-    //     return a ;                 [don't use this approach to understand first 
-    //                                   dry run binary search code]
-    //    }
-    //    else if (nums[a] > target){
-    //     return a - 1 ;
-    //    }
-    //    return a + 1 ;
-    }
-    int binarysearch(int[] arr, int target){
-        int start = 0 ;
-        int end = arr.length - 1 ;
-        while (start <= end){
-            int mid = start + (end - start) / 2 ;
-            if (arr[mid] > target){
-                end = mid - 1 ;
-            } 
-            else if(arr[mid] < target){
-                start = mid + 1 ;
-            } 
-            else{
-                return mid ;
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
-        } 
-        return start ;
+        }
+
+        return left;
     }
 }
