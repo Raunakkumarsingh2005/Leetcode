b4d8d60abc3bc8f8d541564b83af49b87b81c0f8
Raunak Kumar Singh <kumarsinghraunak27@gmail.com>
2026-06-11 13:11:51 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (10.83%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,24 +1,35 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
+        int low = 0;
+        int high = nums.length-1;
+        int mid = 0;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
+        while (low <= high) {
+            mid = low + (high-low) / 2;
+            if (nums[mid] > nums[high]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+                if (low == high) break;
+            }
+        }
 
-            if (nums[mid] == target) return mid;
+        int r1 = binarySearch(nums, target, 0, mid-1);
+        int r2 = binarySearch(nums, target, mid, nums.length-1);
 
-            if (nums[left] <= nums[mid]) { // left sorted
-                if (nums[left] <= target && target < nums[mid]) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
-            } else { // right sorted
-                if (nums[mid] < target && target <= nums[right]) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
+        return r1 == -1 ? r2 : r1;
+    }
+
+    public int binarySearch(int[] nums, int target, int low, int high) {
+        while (low <= high) {
+            int mid = low + (high-low) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                high = mid-1;
+            } else {
+                low = mid + 1;
             }
         }
 
